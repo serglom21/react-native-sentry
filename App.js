76e96848e4a3685443eeb0fff79ab456aad299a9
sergiosentry <109162568+serglom21@users.noreply.github.com>
2024-04-30 14:03:01 -0400

    initial commit

--- App.js
+++ App.js
@@ -1,7 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import * as Sentry from "@sentry/react-native"
+import { Platform } from 'react-native';
+import { breadcrumbsIntegration } from '@sentry/react';
+
+
+Sentry.init({
+  dsn: "",
+  tracesSampleRate: 1.0,
+  integrations: function (integrations) {
+    const defaultIntegrations = integrations.filter(function (integration) {
+    // excluding Breadcrumbs, HttpContext
+        return ["InboundFilters", "FunctionToString", "LinkedErrors", "Dedupe"].includes(integration.name);
+    });
+
+    return defaultIntegrations.concat([breadcrumbsIntegration({ dom: false, history: false, console: true })]);
+  },
+})
+
 export default function App() {
+  console.log('platform is: ', Platform);
+  Sentry.captureException("this is an error")
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
